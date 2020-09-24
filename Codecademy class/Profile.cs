4f89df94ef8564ab1724f08819dc5e8bdaeb7e99
Alex Sarene <sarene.alex@gmail.com>
2020-09-24 08:44:27 -0400

    final commit

---
+++ Codecademy class/Profile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codecademy_class
+{
+    class Profile
+    {
+        private string name;
+        private string inGameName;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] playedGames;
+
+        public Profile(string name, string inGameName, int age, string city, string country, string pronouns = "They/them")
+        {
+            this.name = name;
+            this.inGameName = inGameName;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.playedGames = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name : {name}\nIn-Game Name : {inGameName}\nAge : {age}\nCity : {city}\nCountry : {country}\nPronouns : {pronouns}";
+            bio += "\nGames :\n";
+            foreach (string hobby in playedGames)
+            {
+                bio += $"- {hobby}\n";
+            }
+            return bio;
+        }
+
+        public void SetGames(string[] playedGames)
+        {
+            this.playedGames = playedGames;
+        }
+    }
+}
--- Codecademy class/Program.cs
+++ Codecademy class/Program.cs
@@ -6,7 +6,10 @@ namespace Codecademy_class
     {
         static void Main(string[] args)
         {
-            
+            Profile alex = new Profile("Alex Sarene", "Jakal", 18, "Canfield", "USA", "he/him");
+            string[] hobbyList = { "League of Legends", "Valorant", "CSGO", "Among Us"};
+            alex.SetGames(hobbyList);
+            Console.WriteLine(alex.ViewProfile());
         }
     }
 }
